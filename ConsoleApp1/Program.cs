fa1790cfcfd713c29b9dd502d8d71ef2e3e2d506
Syam802 <syam1920@gmail.com>
2024-12-09 14:23:52 +0530

    git test

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,6 +6,7 @@
         {
             Console.WriteLine("Hello, World!");
             Console.WriteLine("testing");
+            Console.WriteLine("Git test");
             Console.ReadKey();
         }
     }
